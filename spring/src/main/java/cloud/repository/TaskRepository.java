0fb6d8b6f6797bb2a9b60aedcfde0e8463cc03af
KingOfMath <823221784@qq.com>
2020-06-20 23:07:46 +0200

    Delete TaskRepository.java

--- spring/src/main/java/cloud/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package cloud.repository;
-
-import cloud.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task,Long> {
-}
